c86a7ae52b59a6b1aa196e6fe11fe73c06225857
Tom <thomasbkruth@gmail.com>
2018-12-06 01:55:19 -0800

    First Message

---
+++ src/main/java/kafka_first/first/ProducerDemo.java
@@ -0,0 +1,56 @@
+package kafka_first.first;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    public static void main(String[] args) {
+        final Logger logger = LoggerFactory.getLogger(ProducerDemo.class);
+
+        //create producer props
+        Properties props = new Properties();
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+
+        //create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
+
+
+
+        //send data;
+        int i = 0;
+        while(i < 10) {
+            ProducerRecord<String, String> rec =
+                    new ProducerRecord<String, String>("first_topic", "hello from java" + i);
+            producer.send(rec, new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    if(e == null)
+                    {
+                        logger.info("Received new metadata.\n" +
+                                "Topic: " + recordMetadata.topic() + "\n" +
+                                "Partition: " + recordMetadata.partition() + "\n" +
+                                "Offsets: " + recordMetadata.offset() + "\n" +
+                                "Timestamp: " + recordMetadata.timestamp());
+
+                    }
+                    else{
+                        logger.error("Error: ", e);
+                    }
+                }
+            });
+
+            i++;
+        }
+
+        producer.flush();
+        producer.close();
+
+    }
+}
